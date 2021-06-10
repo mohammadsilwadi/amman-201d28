76325e9fc91b67ff3a39e8fcbd6465931944fc7a
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-10 21:40:10 +0300

    STRETCH GOAL

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -138,11 +138,15 @@ testMultiplyArray(testArray2);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array 
+whose first element is the  product of those numbers,and the second element is a string that EXACTLY follows
+ this example and concatenates a message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, 
+use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+ However, you may continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
@@ -152,10 +156,11 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
-}
-
+    let mul1=1
+    for(let i=0;i<5;i++){
+    mul1=multiply(mul1,dynamicArray[i])[0];
+    }return[mul1,`The numbers ${dynamicArray[0]},${dynamicArray[1]},${dynamicArray[2]},${dynamicArray[3]},${dynamicArray[4]} have a product of ${mul1}.`];}
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+ testMultiplyAnyArray(testDynamicArray);
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! 
+// Submit the link to the repo following the instructions in Canvas.
