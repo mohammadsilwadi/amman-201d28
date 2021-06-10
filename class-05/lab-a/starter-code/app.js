cc4fef2a31bccbde6267c3e96465f696b7ed2a43
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-10 18:44:09 +0300

    solve problem4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -64,7 +64,7 @@ let mulit=multiply(a,b);
 let mulit2=multiply(mulit[0],c);
 return[sum3[0],mulit2[0],`${a} and ${b} and ${c} sum to ${sum3[0]}.`,`The product of ${a} and ${b} and ${c} is ${mulit2[0]}.`];
 }
-console.log(sumAndMultiply(4,7,5));
+// console.log(sumAndMultiply(4,7,5));
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
@@ -72,11 +72,16 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument 
+and then returns an array where the first element is the sum of the numbers in the array, and the second element 
+is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + 
+in this function. To do addition, use your sum() 
+function that you've already created. You're 
+going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
@@ -84,12 +89,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
-}
+let sum1=0
+for(let i=0;i<3;i++){
+sum1=sum(sum1,sumArr[i])[0];
+}return[sum1,`${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sum1} is their sum.`];}
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
